63c28fdcaffabaaf82dbd7fe5ae593f69c6cb6ee
Zuhrul Anam <zuhrrlanam@gmail.com>
2024-07-05 23:48:07 +0700

    Initial Commit

---
+++ firstios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firstios
+//
+//  Created by WDT on 05/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firstios/firstiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstiosApp.swift
+//  firstios
+//
+//  Created by WDT on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct firstiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
